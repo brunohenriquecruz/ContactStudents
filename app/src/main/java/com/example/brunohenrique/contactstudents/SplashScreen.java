c6a3716a0c084cfe8c760d0ae06592d449b0e89b
Bruno Henrique <brunohenriquecruz@yahoo.com.br>
2019-04-14 14:51:27 -0300

    Initial commit

---
+++ app/src/main/java/com/example/brunohenrique/contactstudents/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.brunohenrique.contactstudents;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int Time = 5000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, SplashScreen.class);
+                startActivity(i);
+
+                finish();
+            }
+        },Time);
+    }
+}
