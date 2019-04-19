73fcccec3fc5e9f0d675e3003890c06c5aae7069
Bruno Henrique <brunohenriquecruz@yahoo.com.br>
2019-04-19 12:28:07 -0300

    Toolbar e menu

--- app/src/main/java/com/example/brunohenrique/contactstudents/NextScreen.java
+++ app/src/main/java/com/example/brunohenrique/contactstudents/NextScreen.java
@@ -3,19 +3,20 @@ package com.example.brunohenrique.contactstudents;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.Toolbar.OnMenuItemClickListener;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
+import java.text.BreakIterator;
 
+public class NextScreen extends AppCompatActivity implements OnMenuItemClickListener {
 
-        TextView tvMenu;
-
+    TextView tvMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_next_screen);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher);
@@ -29,10 +30,35 @@ public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuIte
 
     }
 
-
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String texto = "";
+        switch (item.getItemId()) {
+            case R.id.menu_add:
+                texto = "CADASTRAR";
+                break;
+            case R.id.menu_ed:
+                texto = "EDITAR";
+                break;
+            case R.id.menu_del:
+                texto = "APAGAR";
+                break;
+            case R.id.menu_help:
+                texto = "AJUDA";
+                break;
+            case android.R.id.home:
+                texto = "Redirecionado";
+                break;
+        }
+        tvMenu.setText(texto);
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
